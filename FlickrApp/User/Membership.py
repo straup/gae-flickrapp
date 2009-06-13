fc091516a0ab64cd166956af1578815756fb3369
Aaron Straup Cope <straup@gmail.com>
2009-06-13 13:31:30 -0700

    add optin/out classes

---
+++ FlickrApp/User/Membership.py
@@ -0,0 +1,34 @@
+from google.appengine.ext import db
+from FlickrApp.Tables import dbFlickrUserMembership
+
+def retrieve (nsid) :
+
+    gql = "SELECT * FROM dbFlickrUserMembership WHERE nsid = :1"
+    res = db.GqlQuery(gql, nsid)
+    return res.get()
+
+def has_user_opted_out (nsid) :
+    
+    user = retrieve(nsid)
+
+    if not user :
+        return False
+    
+    return user.opted_out
+    
+def create (nsid) :
+
+    user = dbFlickrUserMembership()
+    user.nsid = nsid
+    user.opted_out = False
+    user.put()
+    
+    return user
+        
+def opt_in (user) :
+    user.opted_out = False
+    user.put()
+    
+def opt_out (user) :
+    user.opted_out = True
+    user.put()
